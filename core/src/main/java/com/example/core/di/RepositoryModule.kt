d464e4193769c18b5204a752e49e3e6cb1d821c1
Mohamed Ayad <mohamed.ayad7474@gmail.com>
2026-03-14 21:45:17 +0200

    Added IImageHostStrategy and IIssueTrackerStrategy for easily swap and extend with other options later. Added Logger for development logs.

---
+++ core/src/main/java/com/example/core/di/RepositoryModule.kt
@@ -0,0 +1,61 @@
+package com.example.core.di
+
+import android.content.Context
+import com.example.core.BuildConfig
+import com.example.core.feature.bug_reporting.data.repo.BugReportRepository
+import com.example.core.feature.bug_reporting.domain.repo.IBugReportRepository
+import com.example.core.strategies.image.IImageHostStrategy
+import com.example.core.strategies.issue_tracker.IIssueTrackerStrategy
+import com.example.core.strategies.issue_tracker.GoogleSheetsTrackerStrategy
+import com.example.core.strategies.image.ImgBBHostStrategy
+import com.example.core.utils.AndroidImageCompressor
+import com.example.core.utils.IImageCompressor
+import com.example.core_contracts.data_source.remote.INetworkProvider
+import com.google.api.services.sheets.v4.Sheets
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideImageCompressor(@ApplicationContext context: Context): IImageCompressor {
+        return AndroidImageCompressor(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageHostStrategy(
+        networkProvider: INetworkProvider
+    ): IImageHostStrategy {
+        return ImgBBHostStrategy(networkProvider)
+    }
+
+    @Provides
+    @Singleton
+    fun provideIssueTrackerStrategy(
+        sheetsService: Sheets
+    ): IIssueTrackerStrategy {
+        return GoogleSheetsTrackerStrategy(sheetsService, BuildConfig.SPREAD_SHEET_ID)
+    }
+
+    @Provides
+    @Singleton
+    fun provideBugReportRepository(
+        imageCompressor: IImageCompressor,
+        imageHostStrategy: IImageHostStrategy,
+        issueTrackerStrategy: IIssueTrackerStrategy
+    ): IBugReportRepository {
+        return BugReportRepository(
+            imageCompressor = imageCompressor,
+            imageHostStrategy = imageHostStrategy,
+            issueTrackerStrategy = issueTrackerStrategy
+        )
+    }
+}
