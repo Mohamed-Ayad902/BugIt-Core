9644fd65637bb60b19f19263ca9f5657c86265b1
Mohamed Ayad <mohamed.ayad7474@gmail.com>
2026-03-16 18:37:25 +0200

    Added Minimal App for local testing using the ui.

--- core/src/main/java/com/example/core/utils/AndroidImageCompressor.kt
+++ core/src/main/java/com/example/core/utils/AndroidImageCompressor.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.core.net.toUri
+import com.example.core_contracts.extensions.loge
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
@@ -17,16 +18,46 @@ internal class AndroidImageCompressor @Inject constructor(
         return withContext(Dispatchers.IO) {
             try {
                 val uri = uriString.toUri()
-                context.contentResolver.openInputStream(uri)?.use { inputStream ->
-                    val bitmap = BitmapFactory.decodeStream(inputStream)
-                    val outputStream = ByteArrayOutputStream()
+                val contentResolver = context.contentResolver
 
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
-                    outputStream.toByteArray()
+                val options = BitmapFactory.Options().apply {
+                    inJustDecodeBounds = true
                 }
-            } catch (_: Exception) {
+                contentResolver.openInputStream(uri)?.use { inputStream ->
+                    BitmapFactory.decodeStream(inputStream, null, options)
+                }
+
+                options.inSampleSize = calculateInSampleSize(options, 1080, 1920)
+                options.inJustDecodeBounds = false
+
+                val bitmap = contentResolver.openInputStream(uri)?.use { inputStream ->
+                    BitmapFactory.decodeStream(inputStream, null, options)
+                } ?: return@withContext null
+
+                val outputStream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, outputStream)
+                bitmap.recycle()
+
+                outputStream.toByteArray()
+            } catch (e: Exception) {
+                "error while compressing image: $e".loge()
                 null
             }
         }
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        val (height: Int, width: Int) = options.outHeight to options.outWidth
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
     }
 }
